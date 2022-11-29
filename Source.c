292cd7b9bf727c56a3890d8a70eb005005299703
andrnnov <andrnnov64@mail.ru>
2022-11-29 17:09:29 +0300

    add select_sort and insert_sort (with error)

--- Source.c
+++ Source.c
@@ -4,11 +4,109 @@
 #include <locale.h>
 //#include <tchar.h>
 #include <malloc.h>
+#include <time.h>
 
 typedef struct {
-    wchar_t name_file[261];
+    wchar_t name_file[260];
     unsigned long long size_file;
 } file;
+
+int insert_sort(file* ptr, unsigned int count) {
+    unsigned int j;
+    unsigned long long t = 0;
+    unsigned long long* buff;
+    double time_spent = 0.0;
+
+    if ((buff = malloc(sizeof(unsigned long long) * count)) == NULL) {
+        printf("Unable to allocate %llu bytes of memory for character buffer\n", count * sizeof(file));
+        return 0;
+    }
+    for (unsigned int i = 0; i < count; i++) {
+        if (buff[i]) {
+            buff[i] = ptr[i].size_file;
+//            printf("%llu\n", buff[i]);
+        }
+    }
+
+    clock_t begin = clock();
+    for (unsigned int i = 1; i < count; i++)
+    {
+        t = buff[i];
+        j = i - 1;
+        while (t > buff[j] && j >= 0) {
+            buff[j + 1] = buff[j];
+            j--;
+        }
+        buff[j + 1] = t;
+    }
+
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("The elapsed time is %f seconds", time_spent);
+    printf("\n");
+//    for (unsigned int i = 0; i < count; i++)
+//        printf("%llu\n", buff[i]);
+    for (unsigned int i = 0; i < count; i++)
+        for (j = 0; j < count; j++)
+            if (buff[i] == ptr[j].size_file) {
+                printf("%d\t%ls\t %llu\n", i, ptr[j].name_file, ptr[j].size_file);
+                break;
+            }
+            
+    free(buff);
+    return 1;
+}
+
+int select_sort(file* ptr, unsigned int count) {
+    unsigned int a, b, c;
+    int exchange = 0;
+    unsigned long long t = 0;
+    unsigned long long* buff;
+    double time_spent = 0.0;
+
+    if ((buff = malloc(sizeof(unsigned long long) * count)) == NULL) {
+        printf("Unable to allocate %llu bytes of memory for character buffer\n", count * sizeof(file));
+        return 0;
+    }
+    for (unsigned int i = 0; i < count; i++) {
+        if (buff[i]) 
+            buff[i] = ptr[i].size_file;
+    }
+
+    clock_t begin = clock();
+    for (a = 0; a < count - 1; ++a) {
+        exchange = 0;
+        c = a;
+        t = buff[a];
+        for (b = a + 1; b < count; ++b) {
+            if (buff[b] < t) {
+                c = b;
+                t = buff[b];
+                exchange = 1;
+            }
+        }
+        if (exchange) {
+            buff[c] = buff[a];
+            buff[a] = t;
+        }
+    }
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("The elapsed time is %f seconds", time_spent);
+
+    printf("\n");
+    for (a = 0; a < count; a++) 
+        for (b = 0; b < count; b++) 
+            if (buff[a] == ptr[b].size_file) {
+                printf("%d\t%ls\t %llu\n", a, ptr[b].name_file, ptr[b].size_file);
+                break;
+            }
+//        if (i < count)
+//            printf("%llu\n", buff[i]);
+    
+    free(buff);
+    return 1;
+}
 
 int find(wchar_t* PathAndName, unsigned int count)
 {
@@ -16,30 +114,39 @@ int find(wchar_t* PathAndName, unsigned int count)
     HANDLE hf;
     unsigned long long nFileLen = 0;
     file *ptrf;
-    int i = 0;
+    unsigned int i = 0;
  
-    ptrf = (file *)malloc((count) * sizeof(file));
-    if (ptrf == NULL)
+    if ((ptrf = (file*)malloc((count) * (unsigned int)sizeof(file))) == NULL) {
+        printf("Unable to allocate %llu bytes of memory for character buffer\n", count * sizeof(file));
         return 0;
+    }
     hf = FindFirstFile(PathAndName, &FindFileData);
     if (hf != INVALID_HANDLE_VALUE) {
         while (FindNextFile(hf, &FindFileData)) {
-            nFileLen = (FindFileData.nFileSizeHigh * (MAXDWORD + 1)) + FindFileData.nFileSizeLow;
+            nFileLen = (FindFileData.nFileSizeHigh * ((unsigned long long)MAXDWORD + 1)) + FindFileData.nFileSizeLow;
             if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
                 printf("%ls\t Каталог\n", FindFileData.cFileName);
             else {
                 wcscpy_s(ptrf[i].name_file, sizeof(FindFileData.cFileName) / sizeof(wchar_t), FindFileData.cFileName);
  //               printf("sizeof() = %llu\n", (unsigned long long)(sizeof(FindFileData.cFileName) / sizeof(wchar_t)));
-                ptrf[i].size_file = nFileLen;
+                if (i < count)
+                    ptrf[i].size_file = nFileLen;
 //                printf("\n%d\t%ls\t %llu\n", i, ptrf[i].name_file, ptrf[i].size_file);
                 i++;
             }
         }
         for(unsigned int j = 0; j < count; j++)
-            printf("\n%d\t%ls\t %llu\n", j, ptrf[j].name_file, ptrf[j].size_file);
+            printf("%d\t%ls\t %llu\n", j, ptrf[j].name_file, ptrf[j].size_file);
+//        select_sort(ptrf, count);
+        insert_sort(ptrf, count);
         free(ptrf);
         FindClose(hf);
+        printf("%d\n", (unsigned int)sizeof(file));
         system("pause");
+    }
+    else {
+        printf("INVALID HANDLE VALUE!!!\n");
+        return 0;
     }
     return 1;
 }
